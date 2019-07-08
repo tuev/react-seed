85eb13ea7aa61bfb39f1173bed7b8b9f016998f3
Quang Vu <vuquangit@gmail.com>
2019-07-08 09:46:37 +0700

    feat: check commit footer

--- src/Template/Footer/index.js
+++ src/Template/Footer/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 
 export default class index extends Component {
   render () {
-    return (
-      <div />
-    )
+    return <div>Hello, I'm Footer</div>
   }
 }
