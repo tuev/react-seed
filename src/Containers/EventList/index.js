7d3ffa888a1c7121138052bfa576179745d5ef7d
tue <tuevu0171@gmail.com>
2019-07-14 16:30:25 +0700

    test: git test

--- src/Containers/EventList/index.js
+++ src/Containers/EventList/index.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, NavLink } from 'reactstrap'
-import EventItem from '../EventItem'
+// import EventItem from '../EventItem'
 import eventData from './../EventItem/event.json'
 
 export default class EventList extends Component {
-    _renderEventItem = (item, idx) => {
-      const childProps = { ...item }
-      return (
-        <Col key={idx} md='12' sm='6' xs='12'>
-          <div className='contentEvent'><h4>{item.subject}</h4><NavLink className='seeLink'>See more</NavLink>></div>
-          <EventItem {...childProps} />
-        </Col>
-      )
-    };
-    render () {
-      return (
-        <Container>
-          <Row>{eventData.map(this._renderEventItem)}</Row>
-        </Container>
-      )
-    }
+  _renderEventItem = (item, idx) => {
+    // const childProps = { ...item }
+    return (
+      <Col key={idx} md='12' sm='6' xs='12'>
+        <div className='contentEvent'>
+          <h4>{item.subject}</h4>
+          <NavLink className='seeLink'>See more</NavLink>>
+        </div>
+        {/* <EventItem {...childProps} /> */}
+        <span>this is Tue's code</span>
+      </Col>
+    )
+  }
+  render () {
+    return (
+      <Container>
+        <Row>{eventData.map(this._renderEventItem)}</Row>
+      </Container>
+    )
+  }
 }
