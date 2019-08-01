92400295040d08ae631b6c81dc18f17f150d0004
Quang Vu <vuquangit@gmail.com>
2019-08-01 13:39:58 +0700

    fix: fix style of banner event

--- src/Components/EventBanner/index.js
+++ src/Components/EventBanner/index.js
@@ -7,14 +7,17 @@ import './eventBanner.scss'
 const EventBanner = () => (
   <div className='event-banner'>
     <Container>
-      <Row className='event-banner__content d-flex align-items-center'>
-        <Col sm='8' xs='12'>
+      <Row className='event-banner__content d-flex align-items-center justify-content-center'>
+        <Col sm='8' xs='12' className='pl-0 pr-0'>
           <div className='event-banner__content-create-event'>
             Want to create an <span className='event-text'>event</span>?
           </div>
         </Col>
-        <Col sm='4' xs='12'>
-          <div className='event-banner__content-learn-more d-flex align-items-center text-right'>
+        <Col sm='4' xs='12' className='pl-0 pr-0'>
+          <div
+            className='event-banner__content-learn-more
+          d-flex align-items-center justify-content-center justify-content-sm-start text-right'
+          >
             Learn more
             <div className='icon'>
               <FontAwesomeIcon icon={faChevronCircleRight} />
