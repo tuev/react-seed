dd982e5f926fe6c86670dbc047a3ec3eb479b28d
Quang <vuquangit@gmail.com>
2019-08-14 08:26:01 +0700

    feat: use 'switch case' for slider reducer

--- src/Redux/Slider/slider.reducer.js
+++ src/Redux/Slider/slider.reducer.js
@@ -2,35 +2,21 @@ import initSliderStore from './slider.store'
 import * as actionTypes from './slider.actionTypes'
 
 const SliderReducer = (state = initSliderStore, action) => {
-  if (action.type === actionTypes.TOGGLE_PLAY) {
-    const play = slider => {
-      slider.slickPlay()
-    }
-
-    const pause = slider => {
-      slider.slickPause()
-    }
-
+  switch (action.type) {
+  case actionTypes.TOGGLE_PLAY:
+    const play = slider => slider.slickPlay()
+    const pause = slider => slider.slickPause()
     state.isPlay ? pause(state.slider) : play(state.slider)
-
     return { ...state, isPlay: !state.isPlay }
-  }
-
-  if (action.type === actionTypes.INIT_SLIDER) {
-    return {
-      ...state,
-      slider: action.payload
-    }
-  }
-
-  if (action.type === actionTypes.SLICKGOTO) {
+  case actionTypes.INIT_SLIDER:
+    return { ...state, slider: action.payload }
+  case actionTypes.SLICKGOTO:
     const _goToIndex = (slider, index) => slider.slickGoTo(index, true)
     _goToIndex(state.slider, action.payload)
-
     return { ...state }
+  default:
+    return state
   }
-
-  return state
 }
 
 export default SliderReducer
