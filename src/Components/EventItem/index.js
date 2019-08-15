1790d437e7d27ab112243b4c00a78706c91f5fc4
Quang <vuquangit@gmail.com>
2019-08-15 09:20:02 +0700

    feat: modify event item component

--- src/Components/EventItem/index.js
+++ src/Components/EventItem/index.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { Row, Col } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShareAlt, faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import moment from 'moment'
 import './event.scss'
 
 const Item = ({
   image = '',
   alt = '',
-  month = '',
   date = '',
   name = '',
-  content = '',
   location = '',
   price = ''
 }) => {
-  const eventDate = new Date(date)
-  const eventMonth = eventDate.toLocaleString('default', { month: 'short' })
+  const eventMonth = moment(date).format('MMM')
+  const eventDay = moment(date).format('DD')
+  const eventTime = moment(date).format('YYYY/MM/DD hh:mm A')
 
   return (
     <Col lg='4' md='6' sm='12' className='event-item pl-0 pr-3 pb-3'>
@@ -30,14 +30,16 @@ const Item = ({
         </Row>
         <Row className='item__info py-4 px-3 position-relative'>
           <Col xs='2'>
-            <p className='item__info item__info--month'>{month}</p>
-            <p className='item__info item__info--date'>{eventMonth}</p>
+            <p className='item__info item__info--month'>{eventMonth}</p>
+            <p className='item__info item__info--date'>{eventDay}</p>
           </Col>
           <Col xs='10' className='d-flex flex-column align-items-start'>
             <p className='item__info item__info--title'>{name}</p>
-            <p className='item__info item__info--content'>{content}</p>
+            <p className='item__info item__info--time'>{eventTime}</p>
             <p className='item__info item__info--address'>{location}</p>
-            <p className='item__info item__info--price'>{price}</p>
+            <p className='item__info item__info--price'>
+              {price !== 'Free' ? '$' + price : price}
+            </p>
           </Col>
           <div className='item__action d-flex position-absolute'>
             <Col className='pl-0'>
